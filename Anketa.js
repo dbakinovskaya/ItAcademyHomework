cb02421c31173355dba21c4fefab4afb56e4dfd7
Darya Dobysh <d.bakinovskaya@gmail.com>
2022-04-23 14:31:14 +0300

    init: start a first task on JS

---
+++ Anketa.js
@@ -0,0 +1,19 @@
+var surname = prompt('Введите вашу фамилию');
+var name = prompt('Введите ваше имя');
+var secondname = prompt('Введите ваше отчество');
+var age = prompt('Введите ваш возраст в годах');
+var gender = confirm('Ваш пол мужской?');
+
+var yourGender = "женский";
+if (gender) {
+  yourGender = "мужской"
+}
+
+var benefit = "да";
+if ((gender && age < 63) || (!gender && age < 58)) {
+  benefit = "нет"
+}
+
+var anketa = (`Ваше ФИО: ${surname} ${name} ${secondname}. Ваш возраст в годах: ${age}. Ваш возраст в днях: ${Number(age) * 365}. Через пять лет вам будет: ${Number(age) + 5}.  Ваш пол: ${yourGender}. Вы на пенсии: ${benefit}.`);
+
+alert(anketa)
